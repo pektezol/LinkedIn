ff1dc6168c47fcc09046f0eaf8c06de77344d9c3
ceirem <72570708+ceirem@users.noreply.github.com>
2023-05-20 12:28:29 +0300

    navbar Routes done

--- frontend/src/components/Layout/Navbar.vue
+++ frontend/src/components/Layout/Navbar.vue
@@ -17,34 +17,34 @@
           <ul class="navbar-nav ml-auto mt-4">
 
             <li class="nav-item active">
-              <a class="nav-link" href="#">
+              <a class="nav-link" href="/">
                 <img class="transform translate-x-1" src="@/assets/images/nav-home.svg" alt="" srcset="">
                 <p>Home</p>
               </a>
             </li>
 
             <li class="nav-item">
-              <a class="nav-link" href="#">
-                <img class="transform translate-x-6" src="@/assets/images/nav-network.svg" alt="" srcset="">
+              <a class="nav-link" href="/networks">
+                <img class="transform translate-x-6" src="@/assets/images/nav-network.svg" alt="" srcset="" >
                 <p>My Networks</p>
               </a>
             </li>
 
             <li class="nav-item">
-              <a class="nav-link" href="#">
+              <a class="nav-link" href="/jobs">
                 <img class="transform translate-x-px" src="@/assets/images/nav-jobs.svg" alt="" srcset="">
                 <p>Jobs</p>
               </a>
             </li>
 
-            <li class="nav-item">
+            <!-- <li class="nav-item">
               <a class="nav-link" href="#">
                 <i class="fas fa-comment-dots fa-lg text-center">
                   <img class="transform translate-x-6" src="@/assets/images/nav-messaging.svg" alt="" srcset="">
                   <p>Messaging</p>
                 </i>
               </a>
-            </li>
+            </li> -->
 
             <li class="nav-item">
               <a class="nav-link" href="#">
@@ -54,7 +54,7 @@
             </li>
 
             <li class="nav-item mr-3">
-              <a class="nav-link" href="#">
+              <a class="nav-link" href="/profile">
                 <img class="rounded-circle ml-2 text-center"
                   src="https://media-exp1.licdn.com/dms/image/C4D03AQFzRzfBHzBpbA/profile-displayphoto-shrink_100_100/0/1661208519486?e=1669248000&v=beta&t=F_N3_KlkhDP4RbePX7fUwfN9TtrmwSAdb-1OW3VvUA4"
                   style="width: 25px; height: 25px;" />
@@ -62,7 +62,7 @@
                   <div class="w-full text-center">
                     <!--             <img :src="user.data.photoURL" alt="" srcset=""  ref="btnRef" class="transform translate-x-px rounded-full w-7 h-7 ease-linear transition-all duration-150">
  -->
-                    <p class="flex">
+                    <p class="flex" >
                       Me
                       <icon @click="" class="language-selector__label-chevron mx-1 lazy-loaded" aria-hidden="true">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" preserveAspectRatio="xMinYMin meet"
