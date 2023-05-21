456238e3653bdb4584a2c7d580b72e96a361e6d7
Arda Serdar Pektezol <1669855+pektezol@users.noreply.github.com>
2023-05-21 10:57:57 +0300

    feat: all db tables

--- backend/database/init.sql
+++ backend/database/init.sql
@@ -14,10 +14,99 @@ CREATE TABLE users (
     cv TEXT NOT NULL
 );
 
+CREATE TABLE companies (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    industry TEXT NOT NULL,
+    logo TEXT NOT NULL,
+    location TEXT UNIQUE NOT NULL,
+    description TEXT NOT NULL
+);
+
+CREATE TABLE education (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id),
+    school_name TEXT NOT NULL,
+    degree TEXT NOT NULL,
+    field_of_study TEXT NOT NULL,
+    description TEXT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL
+);
+
+
+CREATE TABLE experience (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id),
+    company_id INT NOT NULL REFERENCES companies(id),
+    title TEXT NOT NULL,
+    location TEXT NOT NULL,
+    description TEXT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL
+);
+
+CREATE TABLE jobs (
+    id SERIAL PRIMARY KEY,
+    company_id INT NOT NULL REFERENCES companies(id),
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    location TEXT NOT NULL,
+    type TEXT NOT NULL,
+    date DATE NOT NULL
+);
+
+CREATE TABLE applications (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id),
+    job_id INT NOT NULL REFERENCES jobs(id),
+    status BOOLEAN NOT NULL,
+    date DATE NOT NULL
+);
+
+
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL REFERENCES users(id),
     text TEXT NOT NULL,
     image TEXT NOT NULL,
+    date DATE NOT NULL
+);
+
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id),
+    post_id INT NOT NULL REFERENCES posts(id),
+    comment TEXT NOT NULL,
+    date DATE NOT NULL
+);
+
+CREATE TABLE likes (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id),
+    post_id INT NOT NULL REFERENCES posts(id),
+    date DATE NOT NULL
+);
+
+CREATE TABLE connections (
+    id SERIAL PRIMARY KEY,
+    sender_id INT NOT NULL REFERENCES users(id),
+    reciever_id INT NOT NULL REFERENCES users(id),
+    date DATE NOT NULL
+);
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    sender_id INT NOT NULL REFERENCES users(id),
+    reciever_id INT NOT NULL REFERENCES users(id),
+    message TEXT NOT NULL,
+    date DATE NOT NULL
+);
+
+CREATE TABLE notifications (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id),
+    notification TEXT NOT NULL,
+    status BOOLEAN NOT NULL,
     date DATE NOT NULL
 );
